aafb6bf7bcf96277da73ecb9f489e5ddd205a38e
David Moreira <dvi.moreira@gmail.com>
2019-11-19 21:52:00 -0300

    Login

--- src/layouts/Unauthenticated.vue
+++ src/layouts/Unauthenticated.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        <div class="custom-card">
-            <router-view></router-view>
+        <div class="container">
+            <div class="columns is-centered is-vcentered">
+                <div class="column is-variable is-12-mobile is-12-tablet is-10-desktop is-10-widescreen is-10-fullhd">
+                    <div class="custom-card">
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -21,14 +27,71 @@
         background: -webkit-linear-gradient(55deg, rgba(199,80,192,1) 30%, rgba(67,87,207,1) 100%);
         background: linear-gradient(55deg, rgba(199,80,192,1) 30%, rgba(67,87,207,1) 100%);
         filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#c750c0",endColorstr="#4357cf",GradientType=1);
+        display: grid;
+        align-items: center;
+        height: 100%;
     }
 
     body {
         font-family: 'Roboto', sans-serif;
     }
 
+    .text-center { text-align: center; }
+    .text-left { text-align: left; }
+    .text-right { text-align: right; }
+
+    .custom-card {
+        background: #ffffff;
+        border-radius: 25px;
+    }
+
+    .main-title { color: #000000; }
+
+    .form {
+        .input { background: #E5E4E4; }
+    }
+
+    .button {
+        text-transform: uppercase;
+        &.is-info {
+            background-color: #4656CE;
+            &:hover {
+                opacity: 0.7;
+                background-color: #4656CE;
+            }
+        }
+    }
+
     @media (min-width: 576px) {}
     @media (min-width: 768px) {}
     @media (min-width: 992px) {}
-    @media (min-width: 1200px) {}
+    @media (min-width: 1200px) {
+        .custom-card { padding: 80px 40px; }
+
+        .main-title {
+            font-size: 30px;
+            font-weight: 900;
+            margin: 0 0 30px 0;
+        }
+
+        .form {
+            .input {
+                border: none;
+                border-radius: 50px;
+                height: 50px;
+                padding: 0 15px;
+                &:focus { box-shadow: none; }
+            }
+        }
+
+        .button {
+            border-radius: 50px;
+            width: 100%;
+            height: 50px;
+            font-size: 20px;
+            font-weight: 700;
+            -webkit-transition: all 0.3s;
+            transition: all 0.3s;
+        }
+    }
 </style>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,5 +1,34 @@
 <template>
-    <div>dfgdfg</div>
+    <div>
+        <div class="columns is-centered is-vcentered">
+            <div class="column is-variable is-12-mobile is-12-tablet is-7-desktop is-7-widescreen is-7-fullhd"></div>
+            <div class="column is-variable is-12-mobile is-12-tablet is-5-desktop is-5-widescreen is-5-fullhd">
+                <div class="text-center">
+                    <h1 class="main-title">Painel de Acesso</h1>
+                </div>
+
+                <div class="form text-center">
+                    <b-field>
+                        <b-input type="email" v-model="form.email" placeholder="E-mail"></b-input>
+                    </b-field>
+
+                    <b-field>
+                        <b-input type="password" v-model="form.password" password-reveal placeholder="Senha"></b-input>
+                    </b-field>
+
+                    <b-button type="is-info">Login</b-button>
+                </div>
+
+                <div class="reset-password text-center">
+                    <a href="#">Esqueci minha senha</a>
+                </div>
+
+                <div class="create-account text-center">
+                    <a href="#">Criar uma conta <i class="icofont-long-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,7 +38,9 @@
         name: 'auth',
         components: {},
         data () {
-            return {}
+            return {
+                form: {}
+            }
         },
         methods: {
             async onLogin (email, password) {
@@ -82,4 +113,40 @@
     }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+    .reset-password, .create-account {
+        a {
+            color: #918F8F;
+            -webkit-transition: all 0.3s;
+            transition: all 0.3s;
+            &:hover {
+                opacity: 0.7;
+            }
+        }
+    }
+
+    @media (min-width: 576px) {}
+    @media (min-width: 768px) {}
+    @media (min-width: 992px) {}
+    @media (min-width: 1200px) {
+        .reset-password {
+            a {
+                display: block;
+                margin: 20px 0 80px 0;
+            }
+        }
+
+        .create-account {
+            a {
+                display: block;
+                margin: 0;
+                font-size: 16px;
+                font-weight: 700;
+                i {
+                    font-size: 24px;
+                    vertical-align: middle;
+                }
+            }
+        }
+    }
+</style>
